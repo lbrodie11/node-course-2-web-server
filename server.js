2fdc1197143c5a0d121659f2ba9cf574d814e7e9
Laqwan Brodie <laqwanbrodie@Laqwans-MBP.fios-router.home>
2018-05-21 13:49:08 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -44,13 +45,6 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.get('/maintenance', (req, res) => {
-//   res.render('home.hbs', {
-//     pageTitle: 'Will be right back',
-//     welcomeMessage: 'This site is currently being updated'
-//   });
-// });
-
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
@@ -64,6 +58,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
